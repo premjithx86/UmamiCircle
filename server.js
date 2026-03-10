c453d6d78eefcbf736c41953b2408c9c11cc41d0
premjithx86 <premjithp17@gmail.com>
2026-03-11 03:49:12 +0530

    fix(backend): Configure database connection in server.js and add default MONGO_URI

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
+require("dotenv").config();
+
 const port = process.env.PORT || 8080;
+
+// Connect to Database
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
